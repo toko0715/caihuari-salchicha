a88b79c73ab7b01ab1e7f83f586c9d7956876700
toko0715 <strongsitojheremy@gmail.com>
2025-06-22 17:23:12 -0500

    modified:   hola.py

--- hola.py
+++ hola.py
@@ -2,4 +2,9 @@ print("hello world")
 def greet(name):
     return f"Hello, {name}!"
 def add(a, b):
-    return a + b            
+    return a + b
+
+
+#jheremy :v
+def saludar():
+    print("hola caihuari")
